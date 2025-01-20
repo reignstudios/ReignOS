cf03b4a684dd6b2c42a34c1556644b990be8f431
Andrew <zezba9000@gmail.com>
2025-01-19 19:08:41 -0800

    Added missing C methods

--- Managment/ReignOS.Service.Native/Library.c
+++ Managment/ReignOS.Service.Native/Library.c
@@ -5,7 +5,12 @@ int ioctl_var_arg0(int __fd, unsigned long int __request)
     ioctl(__fd, __request);
 }
 
-int ioctl_var_arg1(int __fd, unsigned long int __request, int __var_arg)
+int ioctl_var_arg1_int(int __fd, unsigned long int __request, int __var_arg)
+{
+    ioctl(__fd, __request, __var_arg);
+}
+
+int ioctl_var_arg1_void(int __fd, unsigned long int __request, void* __var_arg)
 {
     ioctl(__fd, __request, __var_arg);
 }
