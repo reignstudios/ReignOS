4e5a85c975506ba828716460b4544eedb15f1081
Andrew <zezba9000@gmail.com>
2025-01-23 07:45:10 -0800

    test

--- Managment/ReignOS.Bootloader/CheckUpdates.sh
+++ Managment/ReignOS.Bootloader/CheckUpdates.sh
@@ -1,56 +1,61 @@
 #!/bin/bash
 
+run_updates() {
+    # make sure steam is shutdown
+    echo "Shutting down Steam..."
+    steam -Shutdown
+    counter=0
+    while pgrep "steam" >/dev/null; do
+        echo "Waiting for Steam to close..."
+        sleep 1
+        counter=$((counter + 1))
+        if [ "$counter" -ge "10" ]; then
+            echo "Timeout reached. Force closing Steam..."
+            echo "gamer" | sudo pkill "steam"
+            break
+        fi
+    done
+
+    # make sure ReignOS Managment stuff isn't running
+    echo "Waiting for ReignOS Managment to exit..."
+    while pgrep "ReignOS.Bootloader" >/dev/null; do
+        echo "Waiting for ReignOS.Bootloader to close..."
+        sleep 1
+        counter=$((counter + 1))
+        if [ "$counter" -ge "10" ]; then
+            echo "Timeout reached. Force closing ReignOS Managment..."
+            echo "gamer" | sudo pkill "ReignOS.ControlCenter"
+            echo "gamer" | sudo pkill "ReignOS.Service"
+            echo "gamer" | sudo pkill "ReignOS.Bootloader"
+            break
+        fi
+    done
+
+    # update Arch
+    echo ""
+    echo "ReignOS Updating Arch..."
+    echo "gamer" | sudo pacman -Syu --noconfirm
+
+    # update ReignOS Git package
+    echo ""
+    echo "ReignOS Updating Git packages..."
+    cd ~/ReignOS
+    git reset --hard
+    git pull
+    cd Managment
+    echo "ReignOS Building packages..."
+    dotnet publish -r linux-x64 -c Release
+}
+
 # block until shutdown
 if [ "$1" = "-wait-shutdown" ]; then
     dbus-monitor --system "type='signal',interface='org.freedesktop.login1.Manager',member='PrepareForShutdown'" | while read -r line; do
         if [[ $line == *"boolean true"* ]]; then
             echo "Shutdown signal received"
-            exit 0
+            run_updates
+            break
         fi
     done
+else
+    run_updates
 fi
-
-# make sure steam is shutdown
-echo "Shutting down Steam..."
-steam -Shutdown
-counter=0
-while pgrep "steam" >/dev/null; do
-    echo "Waiting for Steam to close..."
-    sleep 1
-    counter=$((counter + 1))
-    if [ "$counter" -ge "10" ]; then
-        echo "Timeout reached. Force closing Steam..."
-        echo "gamer" | sudo pkill "steam"
-        break
-    fi
-done
-
-# make sure ReignOS Managment stuff isn't running
-echo "Waiting for ReignOS Managment to exit..."
-while pgrep "ReignOS.Bootloader" >/dev/null; do
-    echo "Waiting for ReignOS.Bootloader to close..."
-    sleep 1
-    counter=$((counter + 1))
-    if [ "$counter" -ge "10" ]; then
-        echo "Timeout reached. Force closing ReignOS Managment..."
-        echo "gamer" | sudo pkill "ReignOS.ControlCenter"
-        echo "gamer" | sudo pkill "ReignOS.Service"
-        echo "gamer" | sudo pkill "ReignOS.Bootloader"
-        break
-    fi
-done
-
-# update Arch
-echo ""
-echo "ReignOS Updating Arch..."
-echo "gamer" | sudo pacman -Syu --noconfirm
-
-# update ReignOS Git package
-echo ""
-echo "ReignOS Updating Git packages..."
-cd ~/ReignOS
-git reset --hard
-git pull
-cd Managment
-echo "ReignOS Building packages..."
-dotnet publish -r linux-x64 -c Release
