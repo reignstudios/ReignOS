e65354d4de8b44c02d555b645fedbd19a80f3778
Andrew <zezba9000@gmail.com>
2025-01-23 06:43:12 -0800

    test

--- Managment/ReignOS.Bootloader/Launch.sh
+++ Managment/ReignOS.Bootloader/Launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-systemd-inhibit --what=shutdown --who="ReignOS" --why="Preventing shutdown" -- steam -shutdown &
+systemd-inhibit --what=shutdown --who="ReignOS" --why="Preventing shutdown" -- ./CheckUpdates.sh &
 
 # run bootloader
 ./ReignOS.Bootloader $@
@@ -9,12 +9,12 @@ exit_code=$?
 echo "gamer" | sudo pkill "systemd-inhibit"
 
 # run post updater
-if [ $exit_code -eq 9 ]; then
-	chmod +x ./CheckUpdates.sh
-	./CheckUpdates.sh
-
-	# shutdown
-	echo ""
-	echo "ReignOS (shutting down)..."
-	poweroff
-fi
+#if [ $exit_code -eq 9 ]; then
+#	chmod +x ./CheckUpdates.sh
+#	./CheckUpdates.sh
+#
+#	# shutdown
+#	echo ""
+#	echo "ReignOS (shutting down)..."
+#	poweroff
+#fi
