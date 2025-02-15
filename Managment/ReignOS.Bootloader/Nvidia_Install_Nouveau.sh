6fee9e7880711bd2330ee0f315f2d3dc374b68ac
Andrew <zezba9000@gmail.com>
2025-02-15 02:36:59 -0800

    nvidia install scripts

--- Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "TODO: Install Nouveau"
+echo "Uninstalling Nvidia Proprietary drivers"
+echo "gamer" | sudo -S pacman -R nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-prime --noconfirm
+
+echo "Installing Nvidia Nouveau drivers"
+echo "gamer" | sudo -S pacman -S vulkan-nouveau lib32-vulkan-nouveau --noconfirm
+
+reboot
 exit 0
--- Managment/ReignOS.Bootloader/Nvidia_Install_Proprietary.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Install_Proprietary.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "TODO: Install Proprietary"
+echo "Uninstalling Nvidia Nouveau drivers"
+echo "gamer" | sudo -S pacman -R vulkan-nouveau lib32-vulkan-nouveau --noconfirm
+
+echo "Installing Nvidia Proprietary drivers"
+echo "gamer" | sudo -S pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-prime --noconfirm
+
+reboot
 exit 0
