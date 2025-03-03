ab64c9c116fc2f3d2afaeaa6b8fd524bffb56de3
Andrew <zezba9000@gmail.com>
2025-03-03 02:06:49 -0800

    missing git pull in update script

--- Managment/ReignOS.Bootloader/Update.sh
+++ Managment/ReignOS.Bootloader/Update.sh
@@ -24,6 +24,7 @@ echo ""
 echo "ReignOS Updating Git packages..."
 cd /home/gamer/ReignOS
 git reset --hard
+git pull
 cd /home/gamer/ReignOS/Managment
 echo "ReignOS Building packages..."
 dotnet publish -r linux-x64 -c Release
