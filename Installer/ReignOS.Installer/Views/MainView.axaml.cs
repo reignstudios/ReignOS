90b9acd1bf2ca883cb7a6534ed4a928a4044b10d
Andrew <zezba9000@gmail.com>
2025-03-29 18:19:34 -0700

    just close for now

--- Installer/ReignOS.Installer/Views/MainView.axaml.cs
+++ Installer/ReignOS.Installer/Views/MainView.axaml.cs
@@ -595,8 +595,7 @@ public partial class MainView : UserControl
         }
         else
         {
-            ProcessUtil.Run("labwc", "--session bash -c \"sudo gparted\"", wait:false, asAdmin:false);
-            //MainWindow.singleton.Close();// exit so Labwc will open
+            MainWindow.singleton.Close();// exit so Labwc will open
         }
     }
 
