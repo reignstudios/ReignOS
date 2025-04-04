f26b3fb1f0796f9e08efcbd33ce4eb4c28f3fce9
Andrew <zezba9000@gmail.com>
2025-04-03 22:24:58 -0700

    alpha 2

--- Managment/ReignOS.Core/Version.cs
+++ Managment/ReignOS.Core/Version.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace ReignOS.Core;
 public static class VersionInfo
 {
-    public const string version = "0.1.0 alpha";
+    public const string version = "0.2.0 alpha";
 }
