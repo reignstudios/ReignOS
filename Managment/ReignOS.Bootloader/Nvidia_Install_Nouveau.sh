68635eb2286f6248017a8fa52147a4450f59b439
Andrew <zezba9000@gmail.com>
2025-05-07 01:02:43 -0700

    uninstall x11 nvidia

--- Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
@@ -8,6 +8,8 @@ sudo pacman -R --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-setting
 
 echo "Installing Nvidia Nouveau drivers"
 sudo pacman -S --noconfirm vulkan-nouveau lib32-vulkan-nouveau
+sudo pacman -S --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader lib32-libglvnd
+sudo pacman -S --noconfirm xf86-video-nouveau
 sudo mkinitcpio -P
 
 sudo reboot -f
