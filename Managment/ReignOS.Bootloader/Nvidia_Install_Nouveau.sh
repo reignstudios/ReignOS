e14fe573bf14531e4672287598a7189bf2beb768
Andrew <zezba9000@gmail.com>
2025-05-07 02:47:42 -0700

    support lts kernels with nvidia

--- Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Install_Nouveau.sh
@@ -5,6 +5,7 @@ sudo pacman -Syu
 echo "Uninstalling Nvidia Proprietary drivers"
 sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 sudo pacman -R --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-prime
+sudo pacman -R --noconfirm nvidia-lts
 
 echo "Installing Nvidia Nouveau drivers"
 sudo pacman -S --noconfirm vulkan-nouveau lib32-vulkan-nouveau
--- Managment/ReignOS.Bootloader/Nvidia_Install_Proprietary.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Install_Proprietary.sh
@@ -8,6 +8,7 @@ sudo pacman -R --noconfirm xf86-video-nouveau
 
 echo "Installing Nvidia Proprietary drivers"
 sudo pacman -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-prime egl-wayland
+sudo pacman -S --noconfirm nvidia-lts
 sudo mkinitcpio -P
 sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 
