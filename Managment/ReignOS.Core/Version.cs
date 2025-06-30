43d2f819e68af76cece59b08490501d9b3dad653
Andrew <zezba9000@gmail.com>
2025-06-29 19:55:39 -0700

    version

--- Managment/ReignOS.Core/Version.cs
+++ Managment/ReignOS.Core/Version.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace ReignOS.Core;
 public static class VersionInfo
 {
-    public const string version = "0.11.0 alpha";
+    public const string version = "0.11.1 alpha";
 }
