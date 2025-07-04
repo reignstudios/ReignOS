8e87f4d60cfb1d62c204e7b31eccc7f97c9ff8c3
Andrew <zezba9000@gmail.com>
2025-07-03 18:22:08 -0700

    test

--- Managment/ReignOS.Bootloader/Program.cs
+++ Managment/ReignOS.Bootloader/Program.cs
@@ -444,7 +444,7 @@ internal class Program
         }
 
         string displayRezArg = (displayWidth > 0 && displayHeight > 0) ? $" -W {displayWidth} -H {displayHeight}" : "";
-        ProcessUtil.Run($"{gpuArg}gamescope", $"-e -f{useMangoHubArg}{vrrArg}{hdrArg}{rotArg}{displayRezArg} -- ./Start_Gamescope.sh{interfaceArg}{steamGPUArg}{steamDeckArg}", useBash:true, verboseLog:true);// --framerate-limit
+        ProcessUtil.Run($"{gpuArg}gamescope", $"--xwayland-count 2 -e -f{useMangoHubArg}{vrrArg}{hdrArg}{rotArg}{displayRezArg} -- ./Start_Gamescope.sh{interfaceArg}{steamGPUArg}{steamDeckArg}", useBash:true, verboseLog:true);// --framerate-limit
     }
 
     private static void StartCompositor_Weston(bool windowedMode)
