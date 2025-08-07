412ec7a51c9bd5206b9361088989510424b84435
Andrew <zezba9000@gmail.com>
2025-08-07 03:46:33 -0700

    test

--- Managment/ReignOS.Core/Version.cs
+++ Managment/ReignOS.Core/Version.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace ReignOS.Core;
 public static class VersionInfo
 {
-    public const string version = "0.13.9 alpha";
+    public const string version = "0.13.10 alpha";
 }
