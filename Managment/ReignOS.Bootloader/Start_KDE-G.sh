99952c14586494e8e527b676ca0d7eacd49c0552
Andrew <zezba9000@gmail.com>
2025-08-11 02:37:53 -0700

    KDE-G launch stratagy fix to handle closer Monitor

--- Managment/ReignOS.Bootloader/Start_KDE-G.sh
+++ Managment/ReignOS.Bootloader/Start_KDE-G.sh
@@ -1,60 +1,6 @@
 #!/bin/bash
 
-# args
-USE_MANGOHUB=false
-DISABLE_STEAM_GPU=false
-DISABLE_STEAM_DECK=false
-REIGN_MONITOR=false
-for arg in "$@"; do
-    if [ "$arg" = "--use-mangohub" ]; then
-        USE_MANGOHUB=true
-    fi
-
-    if [ "$arg" = "--disable-steam-gpu" ]; then
-        DISABLE_STEAM_GPU=true
-    fi
-
-    if [ "$arg" = "--disable-steam-deck" ]; then
-        DISABLE_STEAM_DECK=true
-    fi
-    
-    if [ "$arg" = "--reign-monitor" ]; then
-        REIGN_MONITOR=true
-    fi
-done
-
-# start steam
-#STEAM_LAUNCH=""
-#if [ "$DISABLE_STEAM_DECK" = "true" ]; then
-#    if [ "$USE_MANGOHUB" = "true" ]; then
-#        STEAM_LAUNCH="mangohud steam -bigpicture -no-cef-sandbox"
-#    else
-#        if [ "$DISABLE_STEAM_GPU" = "true" ]; then
-#            STEAM_LAUNCH="env MESA_GL_VERSION_OVERRIDE=1.3 steam -bigpicture -no-cef-sandbox"
-#        else
-#            STEAM_LAUNCH="steam -bigpicture -no-cef-sandbox"
-#        fi
-#    fi
-#else
-#    if [ "$USE_MANGOHUB" = "true" ]; then
-#        STEAM_LAUNCH="mangohud steam -gamepadui -steamdeck -no-cef-sandbox"
-#    else
-#        if [ "$DISABLE_STEAM_GPU" = "true" ]; then
-#            STEAM_LAUNCH="env MESA_GL_VERSION_OVERRIDE=1.3 steam -gamepadui -steamdeck -no-cef-sandbox"
-#        else
-#            STEAM_LAUNCH="steam -gamepadui -steamdeck -no-cef-sandbox"
-#        fi
-#    fi
-#fi
-
-# start KDE with steam
-#if [ "$REIGN_MONITOR" = "true" ]; then
-  #REIGN_MONITOR_PATH=/home/gamer/ReignOS/Managment/ReignOS.Monitor/bin/Release/net8.0/linux-x64/publish/ReignOS.Monitor
-  #EXIT_MONITOR="wmctrl -c ReignOS.Monitor"
-  #kwin_wayland --lock --xwayland -- bash -c '"$1" & "$2" && "$3"' _ "$REIGN_MONITOR_PATH" "$STEAM_LAUNCH" "$EXIT_MONITOR" &
-#else
-  #kwin_wayland --lock --xwayland -- bash -c "$STEAM_LAUNCH" &
-#fi
+# start KDE
 kwin_wayland --lock --xwayland -- bash -c "/home/gamer/ReignOS/Managment/ReignOS.Bootloader/bin/Release/net8.0/linux-x64/publish/Start_KDE-G_LaunchSteam.sh $@" &
 KWIN_PID=$!
 
@@ -70,7 +16,6 @@ done
 
 # tell KDE to exit
 kill -15 $KWIN_PID 2>/dev/null || true
-sleep 3 # post-kill will sleep too but give extra time
 
 # run post kill
 ./PostKill.sh &
