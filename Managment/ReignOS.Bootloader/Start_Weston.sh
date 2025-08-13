3b4e5cc8eba7add339052a53e9552d148905225c
Andrew <zezba9000@gmail.com>
2025-08-12 17:23:14 -0700

    test

--- Managment/ReignOS.Bootloader/Start_Weston.sh
+++ Managment/ReignOS.Bootloader/Start_Weston.sh
@@ -36,7 +36,7 @@ if [ "$WINDOWED_MODE" = "true" ]; then
 else
     if [ "$DISABLE_STEAM_DECK" = "true" ]; then
         if [ "$USE_MANGOHUB" = "true" ]; then
-            MANGOHUD_CONFIG="horizontal,position=top-center,hud_no_margin,horizontal_stretch,table_columns=100,cpu_power,gpu_power,throttling_status,battery,battery_watt,battery_time,battery_icon=0" \
+            MANGOHUD_CONFIG="horizontal,position=top-center,hud_no_margin,horizontal_stretch,table_columns=100" \
             mangohud steam -bigpicture -no-cef-sandbox
         else
             if [ "$DISABLE_STEAM_GPU" = "true" ]; then
@@ -47,7 +47,7 @@ else
         fi
     else
         if [ "$USE_MANGOHUB" = "true" ]; then
-            MANGOHUD_CONFIG="horizontal,position=top-center,hud_no_margin,horizontal_stretch,table_columns=100,cpu_power,gpu_power,throttling_status,battery,battery_watt,battery_time,battery_icon=0" \
+            MANGOHUD_CONFIG="horizontal,position=top-center,hud_no_margin,horizontal_stretch,table_columns=100" \
             mangohud steam -gamepadui -steamdeck -no-cef-sandbox
         else
             if [ "$DISABLE_STEAM_GPU" = "true" ]; then
