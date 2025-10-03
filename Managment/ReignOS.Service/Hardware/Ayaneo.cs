3e4b2fdbc0dced56112b84ce15749f55655a1e28
Andrew <zezba9000@gmail.com>
2025-10-02 23:53:05 -0700

    menu buttons

--- Managment/ReignOS.Service/Hardware/Ayaneo.cs
+++ Managment/ReignOS.Service/Hardware/Ayaneo.cs
@@ -77,7 +77,7 @@ namespace ReignOS.Service.Hardware
                 }
             }
 
-            if (Program.hardwareType == HardwareType.AyaneoPro || Program.hardwareType == HardwareType.Ayaneo)
+            if (Program.hardwareType == HardwareType.AyaneoPro || Program.hardwareType == HardwareType.AyaneoNextLite || Program.hardwareType == HardwareType.Ayaneo)
             {
                 if (KeyEvent.Pressed(keys, new KeyEvent(input.KEY_RIGHTCTRL, true), new KeyEvent(input.KEY_LEFTMETA, true), new KeyEvent(input.KEY_F12, true)))
                 {
@@ -100,6 +100,7 @@ namespace ReignOS.Service.Hardware
                 Program.hardwareType == HardwareType.AyaneoPlus ||
                 Program.hardwareType == HardwareType.AyaneoFlipDS || Program.hardwareType == HardwareType.AyaneoFlipDS_1S ||
                 Program.hardwareType == HardwareType.AyaneoSlide ||
+                Program.hardwareType == HardwareType.AyaneoNextLite ||
                 Program.hardwareType == HardwareType.Ayaneo
             )
             {
