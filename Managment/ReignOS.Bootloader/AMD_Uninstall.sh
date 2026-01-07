eaa033bf89526d87b757cdd9cea5671695ff969e
Andrew <zezba9000@gmail.com>
2026-01-06 20:32:51 -0800

    Force remove

--- Managment/ReignOS.Bootloader/AMD_Uninstall.sh
+++ Managment/ReignOS.Bootloader/AMD_Uninstall.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
 echo "Uninstalling AMD Proprietary drivers"
-yay -R --noconfirm amf-amdgpu-pro amdgpu-pro-oglp lib32-amdgpu-pro-oglp vulkan-amdgpu-pro lib32-vulkan-amdgpu-pro
-yay -R --noconfirm amf-amdgpu-pro
-yay -R --noconfirm amdgpu-pro-oglp
-yay -R --noconfirm lib32-amdgpu-pro-oglp
-yay -R --noconfirm vulkan-amdgpu-pro
-yay -R --noconfirm lib32-vulkan-amdgpu-pro
+yay -Rdd --noconfirm amf-amdgpu-pro amdgpu-pro-oglp lib32-amdgpu-pro-oglp vulkan-amdgpu-pro lib32-vulkan-amdgpu-pro
 
 echo "Uninstalling AMD VLK drivers"
-sudo pacman -R --noconfirm amdvlk lib32-amdvlk
-sudo pacman -R --noconfirm amdvlk
-sudo pacman -R --noconfirm lib32-amdvlk
+sudo pacman -Rdd --noconfirm amdvlk lib32-amdvlk
 
 echo "Uninstalling AMD MESA drivers"
-sudo pacman -R --noconfirm vulkan-radeon lib32-vulkan-radeon
-sudo pacman -R --noconfirm vulkan-radeon
-sudo pacman -R --noconfirm lib32-vulkan-radeon
+sudo pacman -Rdd --noconfirm vulkan-radeon lib32-vulkan-radeon
 
 exit 0
--- Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
@@ -1,45 +1,26 @@
 #!/bin/bash
 
 sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
+sudo pacman -Rdd --noconfirm nvidia-open # this can block others if not uninstalled first
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo pacman -R --noconfirm nvidia-340xx nvidia-340xx-utils lib32-nvidia-340xx-utils nvidia-340xx-settings
-sudo pacman -R --noconfirm nvidia-340xx
-sudo pacman -R --noconfirm nvidia-340xx-utils
-sudo pacman -R --noconfirm lib32-nvidia-340xx-utils
-sudo pacman -R --noconfirm nvidia-340xx-settings
+sudo pacman -Rdd --noconfirm nvidia-340xx nvidia-340xx-utils lib32-nvidia-340xx-utils nvidia-340xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo pacman -R --noconfirm nvidia-470xx nvidia-470xx-utils lib32-nvidia-470xx-utils nvidia-470xx-settings
-sudo pacman -R --noconfirm nvidia-470xx
-sudo pacman -R --noconfirm nvidia-470xx-utils
-sudo pacman -R --noconfirm lib32-nvidia-470xx-utils
-sudo pacman -R --noconfirm nvidia-470xx-settings
+sudo pacman -Rdd --noconfirm nvidia-470xx nvidia-470xx-utils lib32-nvidia-470xx-utils nvidia-470xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo pacman -R --noconfirm nvidia-580xx nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings
-sudo pacman -R --noconfirm nvidia-580xx
-sudo pacman -R --noconfirm nvidia-580xx-utils
-sudo pacman -R --noconfirm lib32-nvidia-580xx-utils
-sudo pacman -R --noconfirm nvidia-580xx-settings
+sudo pacman -Rdd --noconfirm nvidia-580xx nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo pacman -R --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
-sudo pacman -R --noconfirm nvidia
-sudo pacman -R --noconfirm nvidia-utils
-sudo pacman -R --noconfirm lib32-nvidia-utils
-sudo pacman -R --noconfirm nvidia-settings
-
-sudo pacman -R --noconfirm nvidia-lts
-sudo pacman -R --noconfirm nvidia-prime
-sudo pacman -R --noconfirm egl-gbm
-sudo pacman -R --noconfirm egl-wayland
+sudo pacman -Rdd --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-open
+sudo pacman -Rdd --noconfirm nvidia-lts
+sudo pacman -Rdd --noconfirm nvidia-prime
+sudo pacman -Rdd --noconfirm egl-gbm
+sudo pacman -Rdd --noconfirm egl-wayland
 
 echo "Uninstalling Nvidia Nouveau drivers"
-sudo pacman -R --noconfirm vulkan-nouveau lib32-vulkan-nouveau
-sudo pacman -R --noconfirm vulkan-nouveau
-sudo pacman -R --noconfirm lib32-vulkan-nouveau
-
-sudo pacman -R --noconfirm xf86-video-nouveau
+sudo pacman -Rdd --noconfirm vulkan-nouveau lib32-vulkan-nouveau
+sudo pacman -Rdd --noconfirm xf86-video-nouveau
 
 exit 0
