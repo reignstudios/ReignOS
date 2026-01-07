20421ad9e893495d04b5868d1bff403397afef15
Andrew <zezba9000@gmail.com>
2026-01-06 20:23:22 -0800

    ensure removal

--- Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-echo "Uninstalling Nvidia Proprietary drivers"
 sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
+
+echo "Uninstalling Nvidia Proprietary drivers"
 sudo pacman -R --noconfirm nvidia-340xx nvidia-340xx-utils lib32-nvidia-340xx-utils nvidia-340xx-settings
+sudo pacman -R --noconfirm nvidia-340xx
+sudo pacman -R --noconfirm nvidia-340xx-utils
+sudo pacman -R --noconfirm lib32-nvidia-340xx-utils
+sudo pacman -R --noconfirm nvidia-340xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 sudo pacman -R --noconfirm nvidia-470xx nvidia-470xx-utils lib32-nvidia-470xx-utils nvidia-470xx-settings
+sudo pacman -R --noconfirm nvidia-470xx
+sudo pacman -R --noconfirm nvidia-470xx-utils
+sudo pacman -R --noconfirm lib32-nvidia-470xx-utils
+sudo pacman -R --noconfirm nvidia-470xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 sudo pacman -R --noconfirm nvidia-580xx nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings
+sudo pacman -R --noconfirm nvidia-580xx
+sudo pacman -R --noconfirm nvidia-580xx-utils
+sudo pacman -R --noconfirm lib32-nvidia-580xx-utils
+sudo pacman -R --noconfirm nvidia-580xx-settings
 
 echo "Uninstalling Nvidia Proprietary drivers"
-sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 sudo pacman -R --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
+sudo pacman -R --noconfirm nvidia
+sudo pacman -R --noconfirm nvidia-utils
+sudo pacman -R --noconfirm lib32-nvidia-utils
+sudo pacman -R --noconfirm nvidia-settings
+
 sudo pacman -R --noconfirm nvidia-lts
 sudo pacman -R --noconfirm nvidia-prime
 sudo pacman -R --noconfirm egl-gbm
@@ -22,6 +37,9 @@ sudo pacman -R --noconfirm egl-wayland
 
 echo "Uninstalling Nvidia Nouveau drivers"
 sudo pacman -R --noconfirm vulkan-nouveau lib32-vulkan-nouveau
+sudo pacman -R --noconfirm vulkan-nouveau
+sudo pacman -R --noconfirm lib32-vulkan-nouveau
+
 sudo pacman -R --noconfirm xf86-video-nouveau
 
 exit 0
