26d536f15c2e8eaba131db8f11e17c2b7f50f2f9
Andrew <zezba9000@gmail.com>
2026-01-06 20:48:17 -0800

    Fix more uninstall old Nvidia dependencies causing install issues

--- Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
+++ Managment/ReignOS.Bootloader/Nvidia_Uninstall.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# stop services
 sudo systemctl disable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
-sudo pacman -Rdd --noconfirm nvidia-open # this can block others if not uninstalled first
 
+# this can block others if not uninstalled first
+sudo pacman -Rdd --noconfirm nvidia-open
+sudo pacman -Rdd --noconfirm libxnvctrl
+sudo pacman -Rdd --noconfirm nvidia-lts
+sudo pacman -Rdd --noconfirm nvidia-prime
+sudo pacman -Rdd --noconfirm egl-gbm
+sudo pacman -Rdd --noconfirm egl-wayland
+
+# uninstall main packages
 echo "Uninstalling Nvidia Proprietary drivers"
 sudo pacman -Rdd --noconfirm nvidia-340xx nvidia-340xx-utils lib32-nvidia-340xx-utils nvidia-340xx-settings
 
@@ -14,10 +23,6 @@ sudo pacman -Rdd --noconfirm nvidia-580xx nvidia-580xx-utils lib32-nvidia-580xx-
 
 echo "Uninstalling Nvidia Proprietary drivers"
 sudo pacman -Rdd --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-open
-sudo pacman -Rdd --noconfirm nvidia-lts
-sudo pacman -Rdd --noconfirm nvidia-prime
-sudo pacman -Rdd --noconfirm egl-gbm
-sudo pacman -Rdd --noconfirm egl-wayland
 
 echo "Uninstalling Nvidia Nouveau drivers"
 sudo pacman -Rdd --noconfirm vulkan-nouveau lib32-vulkan-nouveau
