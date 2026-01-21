fb50840c87c67c0924de52d275d785e7a3eb1dd8
Andrew <zezba9000@gmail.com>
2026-01-20 16:31:13 -0800

    Update AvaloniaUI

--- Managment/ReignOS.Core/Version.cs
+++ Managment/ReignOS.Core/Version.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace ReignOS.Core;
 public static class VersionInfo
 {
-    public const string version = "0.19.2 alpha";
+    public const string version = "0.19.3 alpha";
 }
