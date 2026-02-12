0b5bdc9f78e3c5be202f6818c7794b319cd28217
Andrew <zezba9000@gmail.com>
2026-02-11 22:32:50 -0800

    add gamepad ref

--- Managment/ReignOS.Service/VirtualGamepad.cs
+++ Managment/ReignOS.Service/VirtualGamepad.cs
@@ -196,3 +196,74 @@ Input driver version is 1.0.1
        Event code 90 (FF_SINE)
        Event code 96 (FF_GAIN)
 */
+
+/*Input driver version is 1.0.1
+   Input device ID: bus 0x3 vendor 0x45e product 0xb00 version 0x515
+   Input device name: "Microsoft X-Box One Elite 2 pad"
+   Supported events:
+     Event type 0 (EV_SYN)
+     Event type 1 (EV_KEY)
+       Event code 304 (BTN_SOUTH)
+       Event code 305 (BTN_EAST)
+       Event code 307 (BTN_NORTH)
+       Event code 308 (BTN_WEST)
+       Event code 310 (BTN_TL)
+       Event code 311 (BTN_TR)
+       Event code 314 (BTN_SELECT)
+       Event code 315 (BTN_START)
+       Event code 316 (BTN_MODE)
+       Event code 317 (BTN_THUMBL)
+       Event code 318 (BTN_THUMBR)
+       Event code 708 (BTN_TRIGGER_HAPPY5)
+       Event code 709 (BTN_TRIGGER_HAPPY6)
+       Event code 710 (BTN_TRIGGER_HAPPY7)
+       Event code 711 (BTN_TRIGGER_HAPPY8)
+     Event type 3 (EV_ABS)
+       Event code 0 (ABS_X)
+         Value  -2653
+         Min   -32768
+         Max    32767
+         Fuzz      16
+         Flat     128
+       Event code 1 (ABS_Y)
+         Value   -718
+         Min   -32768
+         Max    32767
+         Fuzz      16
+         Flat     128
+       Event code 2 (ABS_Z)
+         Value      0
+         Min        0
+         Max     1023
+       Event code 3 (ABS_RX)
+         Value  -1171
+         Min   -32768
+         Max    32767
+         Fuzz      16
+         Flat     128
+       Event code 4 (ABS_RY)
+         Value   1679
+         Min   -32768
+         Max    32767
+         Fuzz      16
+         Flat     128
+       Event code 5 (ABS_RZ)
+         Value      0
+         Min        0
+         Max     1023
+       Event code 16 (ABS_HAT0X)
+         Value      0
+         Min       -1
+         Max        1
+       Event code 17 (ABS_HAT0Y)
+         Value      0
+         Min       -1
+         Max        1
+     Event type 21 (EV_FF)
+       Event code 80 (FF_RUMBLE)
+       Event code 81 (FF_PERIODIC)
+       Event code 88 (FF_SQUARE)
+       Event code 89 (FF_TRIANGLE)
+       Event code 90 (FF_SINE)
+       Event code 96 (FF_GAIN)
+   Properties:*/
