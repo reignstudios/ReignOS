4f985c83a4beeeed86983f23541a9ae9a1d597a5
Andrew <zezba9000@gmail.com>
2026-02-15 22:13:25 -0800

    version bump

--- Managment/ReignOS.Core/Version.cs
+++ Managment/ReignOS.Core/Version.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace ReignOS.Core;
 public static class VersionInfo
 {
-    public const string version = "0.20.4 alpha";
+    public const string version = "0.20.5 alpha";
 }
