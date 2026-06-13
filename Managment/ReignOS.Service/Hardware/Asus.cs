e727473ebff3bf4ccc1379348b1241657dfc7c45
Andrew <zezba9000@gmail.com>
2026-06-12 18:43:22 -0700

    test

--- Managment/ReignOS.Service/Hardware/Asus.cs
+++ Managment/ReignOS.Service/Hardware/Asus.cs
@@ -48,7 +48,7 @@ namespace ReignOS.Service.Hardware
             {
                 isEnabled = true;
                 initGamepad = true;
-                preferInputDevice = Program.hardwareType == HardwareType.RogXboxAllyX || Program.hardwareType == HardwareType.RogXboxAlly;
+                preferInputDevice = true;//Program.hardwareType == HardwareType.RogXboxAllyX || Program.hardwareType == HardwareType.RogXboxAlly;
                 vid = 0x0b05;
                 pid = 0x1b4c;
             }
